a15111f614068847a99f19c81505bf451c9fb2b0
Arjun Reddy <70253095+Arjunreddypulugu@users.noreply.github.com>
2024-05-31 01:55:16 -0400

    Add files via upload

---
+++ chatPDF/htmlTemplate.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://img.freepik.com/premium-photo/small-boy-colorful-background-funny-cartoon-character-school-kid-3d-generative-ai_58409-28880.jpg?w=996">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://i.etsystatic.com/39477223/r/il/58f254/4621727445/il_1588xN.4621727445_mbda.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
